9aca56abf4c6a2ab6795a586ab0b63d65efac8b5
William Y. Feng <womogenes2@gmail.com>
2022-06-29 17:05:33 -0700

    Remove camera stuff

--- js/draw.js
+++ js/draw.js
@@ -1,19 +1,5 @@
-import { keys } from './keys.js';
-
 const draw = () => {
-  background(16);
-  cam.update();
-  keys();
-
-  push();
-  translate(cam.x, cam.y);
-  scale(cam.zoom);
-
-  fill('#80abf8');
-  noStroke();
-  ellipse(0, 0, 800, 800);
-
-  pop();
+  
 };
 
 export { draw };
--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,4 @@
 import { draw } from './draw.js';
-import { Cam } from './Cam.js';
-import './mouse.js';
 
 // Globals
 window.Vector = p5.Vector;
@@ -10,8 +8,6 @@ window.setup = () => {
   canvas.parent(document.querySelector('#sketch-container'));
   pixelDensity(1);
   ellipseMode('center');
-
-  window.cam = new Cam(width / 2, height / 2, 1, 0.5, 0.5); // Probably want to move this into config file later
 };
 
 window.windowResized = () => {
